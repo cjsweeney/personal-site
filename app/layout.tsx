068b8090152897c2b6e650751069330a28f566f0
Connor Sweeney <sweeney.connorj@gmail.com>
2025-03-15 20:06:56 -0400

    gogle anltycis

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from 'next'
 import './globals.css'
+import Script from 'next/script'
 
 export const metadata: Metadata = {
   title: 'Connor Sweeney',
@@ -13,6 +14,20 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-T22EXVNX5D"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-T22EXVNX5D');
+          `}
+        </Script>
+      </head>
       <body>{children}</body>
     </html>
   )
