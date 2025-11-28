71cc23a1feb66d4a6cc744fce5643bcb65d32c91
Connor Sweeney <sweeney.connorj@gmail.com>
2025-11-28 14:20:37 -0500

    stealth proactive

--- app/page.tsx
+++ app/page.tsx
@@ -73,7 +73,7 @@ export default function Home() {
           </span>
         </div>
         <div className="investment-item">
-          <a href="">Proactive Labs</a>
+          <p>Proactive Labs</p>
           <span className="investment-description">AI Chief of Staff</span>
         </div>
       <div className="investment-item">
