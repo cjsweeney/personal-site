087168c173c63ff7a9ee168429b0518873019535
Connor Sweeney <sweeney.connorj@gmail.com>
2026-06-01 00:01:08 -0400

    foix typos 2

--- app/page.tsx
+++ app/page.tsx
@@ -24,7 +24,9 @@ export default function Home() {
 
       <p>
         Building <a href= "https://www.callbaba.com" className="bio-link">Baba</a>.
+       <p> 
         <br />
+        <p>
         Previously chief of staff to <a href="https://twitter.com/lucy_guo/status/1572660635187531777?s=20&t=UHhW1GuohBblbSBuRGl-4g" className="bio-link">Lucy Guo</a>. 
         Employee #2 at <a href="https://blog.passes.com/passes-announces-40m-series-a/" className="bio-link">Passes</a>.
       </p>
